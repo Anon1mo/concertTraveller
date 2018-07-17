e09f8eb1715eca0bb47bb5e11b5f3434e7d06015
anon1m <lotropj@gmail.com>
2018-07-17 21:14:40 +0200

    dodane modele i routes z autoryzacja i ofertami

--- backend/index.js
+++ backend/index.js
@@ -4,27 +4,47 @@ const open = require('open');
 const express = require('express');
 const webpack = require('webpack');
 const config = require('../webpack.dev');
+const mongoose = require('mongoose');
+require('express-async-errors'); // for handling async errors, throw them to error.js
+
+const events = require('./routes/events');
+const users = require('./routes/users');
+const offers = require('./routes/offers');
+const error = require('./middleware/error');
 
 const app = express();
-const compiler = webpack(config);
-
 const port = process.env.PORT || 3000;
 
-//webpack
-app.use(require('webpack-dev-middleware')(compiler, {
-	hot: true,
-	noInfo: true,
-	publicPath: config.output.publicPath
-}));
-app.use(require('webpack-hot-middleware')(compiler));
-
 app.use(express.static(path.join(__dirname, '../dist')));
+
+mongoose.connect('mongodb://localhost/concertTraveller')
+	.then(() => console.log('Connected to mongoDB'))
+	.catch(() => console.error('Could not connect to mongoDB'))
+
+require('./models/sampleData')();
+
+app.use(express.json());
+app.use('/api/events', events);
+app.use('/api/users', users);
+app.use('/api/offers', offers);
+
+app.use(error);
+// const compiler = webpack(config);
+// //webpack
+// app.use(require('webpack-dev-middleware')(compiler, {
+// 	hot: true,
+// 	noInfo: true,
+// 	publicPath: config.output.publicPath
+// }));
+// app.use(require('webpack-hot-middleware')(compiler));
+
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 app.listen(port, error => {
 	if(error) {
 		console.log(error);
 	} else {
-		open(`http://localhost:${port}`);
+		//open(`http://localhost:${port}`);
+		console.log(`Listening on port ${port}`);
 	}
 });
