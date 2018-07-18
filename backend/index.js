d53f66b9d903d38188be1375970dc69a8062e626
anon1m <lotropj@gmail.com>
2018-07-18 21:23:44 +0200

    Dodano usuwanie ofert, dodawanieie i usuwanie uzytkownika z oferty

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ require('express-async-errors'); // for handling async errors, throw them to err
 const events = require('./routes/events');
 const users = require('./routes/users');
 const offers = require('./routes/offers');
+const auth = require('./routes/auth');
 const error = require('./middleware/error');
 
 const app = express();
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use('/api/events', events);
 app.use('/api/users', users);
 app.use('/api/offers', offers);
+app.use('/api/auth', auth);
 
 app.use(error);
 // const compiler = webpack(config);
