1ee80f7e124771d47077e4cf70e206e1000fe1ca
anon1m <lotropj@gmail.com>
2018-10-31 22:54:42 +0100

    changed base URL for local app

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://concerttraveller.herokuapp.com/api';
+axios.defaults.baseURL = 'http://localhost:4000/api';
 
 axios.interceptors.response.use(null, error => {
 	const expectedError =
